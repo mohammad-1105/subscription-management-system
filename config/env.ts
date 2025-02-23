e42fdd5e3a2b49c9761ca9375d65e3519e8f2be8
Mohammad Aman <mohammadamaan1105@gmail.com>
2025-02-23 13:32:57 +0545

    (feat): add CORS and cookie parsing middleware

--- config/env.ts
+++ config/env.ts
@@ -4,4 +4,5 @@ config({
   path: `.env.${process.env.NODE_ENV} || "development"}.local`,
 });
 
-export const { PORT, NODE_ENV, MONGODB_URI } = process.env;
+export const { PORT, NODE_ENV, MONGODB_URI, ALLOWED_CORS_ORIGINS } =
+  process.env;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,38 @@
+import { ALLOWED_CORS_ORIGINS } from "../config/env";
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.middleware";
+
+if (!ALLOWED_CORS_ORIGINS)
+  throw new Error(
+    "Please define the ALLOWED_CORS_ORIGINS environment variable inside .env<development/production>.local"
+  );
 
 const app = express();
+
+// express built-in middlewares
+app.use(express.json({ limit: "50kb" }));
+app.use(express.urlencoded({ extended: true, limit: "50kb" }));
+
+// cookie parser middleware to parse cookies
+app.use(cookieParser());
+
+// cors middleware
+app.use(
+  cors({
+    origin:
+      ALLOWED_CORS_ORIGINS === "*"
+        ? "*" // This might give CORS error for some origins due to credentials set to true
+        : ALLOWED_CORS_ORIGINS?.split(","), // For multiple cors origin for production.
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Accept", "Authorization"],
+
+    maxAge: 86400,
+    optionsSuccessStatus: 200,
+  })
+);
 
 // routes import
 import authRouter from "./routes/auth.routes";
@@ -11,5 +43,25 @@ import subscriptionRouter from "./routes/subscription.routes";
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hello World!",
+  });
+});
+
+/*
+ * Error Handling Middleware:
+ * --------------------------------------------
+ * 1. This middleware is defined with four parameters: (err, req, res, next).
+ * 2. It is placed at the end of the middleware stack to catch any errors that
+ *    occur in the preceding routes or middleware.
+ * 3. When an error is passed using next(err), Express skips all regular middleware
+ *    and goes directly to this error handler.
+ * 4. This centralized error handling ensures consistent logging and response to errors.
+ * 5. Placing it last prevents subsequent middleware from interfering with error handling.
+ */
+app.use(errorMiddleware);
 
 export default app;
